e406310d45c4e2d0467f6631a1cc64906ffedcfc
Mohit Jaiswal <mohitjaiswal819@gmail.com>
2022-04-06 09:19:52 +0530

    Shell Files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script must contain a series of commands to setup dependencies of your project to make it work.
 # Eg:- For Node.js project;
-#       npm install
+      npm install
 # Eg:- For Java Gradle Project;
 #       ./gradlew build
 # Eg:- For Python Project;
