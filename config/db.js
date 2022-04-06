94413b4ba8c8453afa6af514f997674a859ef3b8
Mohit Jaiswal <mohitjaiswal819@gmail.com>
2022-04-06 10:16:09 +0530

    loacl DB

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect("mongodb://storybookuser:DHQTyI77H4ulbnYH@cluster0-shard-00-00.dtdnj.mongodb.net:27017,cluster0-shard-00-01.dtdnj.mongodb.net:27017,cluster0-shard-00-02.dtdnj.mongodb.net:27017/Tank?ssl=true&replicaSet=atlas-qn3dku-shard-0&authSource=admin&retryWrites=true&w=majority", {
+        const connect = await mongoose.connect("mongodb://localhost/Tank", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
